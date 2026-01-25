2c8cee52e7b6349b570753113f7a7c6318a722f1
aktasilyas <aktasilyas875@gmail.com>
2026-01-26 00:40:27 +0300

    feat(settings): add AppSettings entity

--- example_app/lib/features/settings/domain/entities/app_settings.dart
+++ example_app/lib/features/settings/domain/entities/app_settings.dart
@@ -1,1 +1,61 @@
-// TODO: Implement
+import 'package:equatable/equatable.dart';
+
+enum AppThemeMode { light, dark, system }
+enum AppLanguage { tr, en }
+enum PaperSize { a4, a5, letter }
+
+class AppSettings extends Equatable {
+  final AppThemeMode themeMode;
+  final AppLanguage language;
+  final PaperSize defaultPaperSize;
+  final bool autoSaveEnabled;
+
+  const AppSettings({
+    this.themeMode = AppThemeMode.system,
+    this.language = AppLanguage.tr,
+    this.defaultPaperSize = PaperSize.a4,
+    this.autoSaveEnabled = true,
+  });
+
+  AppSettings copyWith({
+    AppThemeMode? themeMode,
+    AppLanguage? language,
+    PaperSize? defaultPaperSize,
+    bool? autoSaveEnabled,
+  }) {
+    return AppSettings(
+      themeMode: themeMode ?? this.themeMode,
+      language: language ?? this.language,
+      defaultPaperSize: defaultPaperSize ?? this.defaultPaperSize,
+      autoSaveEnabled: autoSaveEnabled ?? this.autoSaveEnabled,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'themeMode': themeMode.name,
+    'language': language.name,
+    'defaultPaperSize': defaultPaperSize.name,
+    'autoSaveEnabled': autoSaveEnabled,
+  };
+
+  factory AppSettings.fromJson(Map<String, dynamic> json) {
+    return AppSettings(
+      themeMode: AppThemeMode.values.firstWhere(
+        (e) => e.name == json['themeMode'],
+        orElse: () => AppThemeMode.system,
+      ),
+      language: AppLanguage.values.firstWhere(
+        (e) => e.name == json['language'],
+        orElse: () => AppLanguage.tr,
+      ),
+      defaultPaperSize: PaperSize.values.firstWhere(
+        (e) => e.name == json['defaultPaperSize'],
+        orElse: () => PaperSize.a4,
+      ),
+      autoSaveEnabled: json['autoSaveEnabled'] ?? true,
+    );
+  }
+
+  @override
+  List<Object?> get props => [themeMode, language, defaultPaperSize, autoSaveEnabled];
+}
